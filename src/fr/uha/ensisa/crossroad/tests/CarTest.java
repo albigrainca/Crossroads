ec818573e0d94bd0de20a490b593b6a36b8d0542
GoKeRjee <habbgoker@outlook.fr>
2023-12-07 16:42:30 +0100

    work in progress - 4 lights now

--- src/fr/uha/ensisa/crossroad/tests/CarTest.java
+++ src/fr/uha/ensisa/crossroad/tests/CarTest.java
@@ -1,2 +1,60 @@
-package fr.uha.ensisa.crossroad.tests;public class CarTest {
+package fr.uha.ensisa.crossroad.tests;
+
+import fr.uha.ensisa.crossroad.app.Car;
+import fr.uha.ensisa.crossroad.ui.ImageLoader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    private Car car;
+    private List<Car> carList;
+
+    @BeforeEach
+    void setUp() {
+        BufferedImage carImage = ImageLoader.resizeImage(ImageLoader.loadImage("car.png"), 50, 50);
+        car = new Car(0, 0, 0, carImage);
+        carList = new ArrayList<>();
+    }
+
+    @Test
+    void testCarCreation() {
+        assertNotNull(car, "La voiture doit être créée");
+        assertNotNull(car.getImage(), "L'image de la voiture ne doit pas être nulle");
+    }
+
+    @Test
+    void testCarMovement() {
+        int initialX = car.getX();
+        int initialY = car.getY();
+        car.move();
+        assertTrue(car.getX() != initialX || car.getY() != initialY, "La voiture doit se déplacer");
+    }
+
+    @Test
+    void testCarDirection() {
+        assertEquals(0, car.getDirection(), "La direction de la voiture doit être correcte");
+    }
+
+    @Test
+    void testCrossTrafficLight() {
+        assertFalse(car.hasCrossedLight(), "La voiture ne doit pas avoir franchi le feu initialement");
+        car.crossTrafficLight();
+        assertTrue(car.hasCrossedLight(), "La voiture doit avoir franchi le feu après l'appel de crossTrafficLight()");
+    }
+
+    @Test
+    void testCarAdditionAndRemovalFromList() {
+        carList.add(car);
+        assertEquals(1, carList.size(), "La liste de voitures doit contenir la voiture ajoutée");
+
+        carList.remove(car);
+        assertEquals(0, carList.size(), "La liste de voitures doit être vide après la suppression de la voiture");
+    }
 }
--- src/fr/uha/ensisa/crossroad/tests/TrafficLightTest.java
+++ src/fr/uha/ensisa/crossroad/tests/TrafficLightTest.java
@@ -1,2 +1,47 @@
-package fr.uha.ensisa.crossroad.tests;public class TrafficLightTest {
+package fr.uha.ensisa.crossroad.tests;
+
+import fr.uha.ensisa.crossroad.app.TrafficLight;
+import fr.uha.ensisa.crossroad.ui.ImageLoader;
+import org.junit.jupiter.api.Test;
+
+import java.awt.image.BufferedImage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrafficLightTest {
+
+    BufferedImage greenLight = ImageLoader.resizeImage(ImageLoader.loadImage("green.jpg"), 56, 56);
+    BufferedImage redLight = ImageLoader.resizeImage(ImageLoader.loadImage("red.jpg"), 56, 56);
+
+    @Test
+    void testTrafficLightChange() {
+        TrafficLight trafficLight = new TrafficLight(6, 3, redLight, greenLight);
+        trafficLight.setGreen(true);
+        assertTrue(trafficLight.isGreen(), "Le feu doit être vert");
+        trafficLight.setGreen(false);
+        assertFalse(trafficLight.isGreen(), "Le feu doit être rouge");
+    }
+
+    @Test
+    void testTrafficLightPosition() {
+        TrafficLight trafficLight = new TrafficLight(6, 3, redLight, greenLight);
+        assertEquals(6, trafficLight.getX(), "La position X doit être 6");
+        assertEquals(3, trafficLight.getY(), "La position Y doit être 3");
+    }
+
+    @Test
+    void testTrafficLightImages() {
+        TrafficLight trafficLight = new TrafficLight(6, 3, redLight, greenLight);
+        trafficLight.setGreen(true);
+        assertSame(greenLight, trafficLight.getCurrentImage(), "L'image doit être celle du feu vert");
+
+        trafficLight.setGreen(false);
+        assertSame(redLight, trafficLight.getCurrentImage(), "L'image doit être celle du feu rouge");
+    }
+
+    @Test
+    void testImageLoading() {
+        assertNotNull(greenLight, "L'image du feu vert ne doit pas être nulle");
+        assertNotNull(redLight, "L'image du feu rouge ne doit pas être nulle");
+    }
 }
